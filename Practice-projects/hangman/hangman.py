3869917fc88e19a0e38ba7dfe207a5efd8f73087
MorganDara <82704477+MorganDara@users.noreply.github.com>
2021-09-18 21:23:41 +0200

    Update hangman.py

--- Practice-projects/hangman/hangman.py
+++ Practice-projects/hangman/hangman.py
@@ -1,5 +1,6 @@
 from random import choice
 from english_words import words
+from os import system
 
 HANGMANPICS = ['''
   +---+
@@ -62,34 +63,43 @@ TITLE = """
                     __/ |                      
                    |___/           
 """
+def print_game_state(message, picture, word):
+  system("cls")
+  print(f"{word}\n\n{message}\n{picture}\n")
 
 def game():
   print(TITLE)
-  guess_word = choice(words)
-  user_word = "*" * len(guess_word)
+  system("pause")
+  guess_word = choice(words).lower()
+  user_word = "_" * len(guess_word)
 
   index = 0
+  print_game_state("", HANGMANPICS[index], user_word)
   while index < (len(HANGMANPICS) - 1) and user_word != guess_word:
-    print(f"\nYour guess: {user_word}\n")
-    letter = input("Guess a letter: ")
+    message = ""
+    letter = input("Guess a letter: ").lower()
     if letter in guess_word and letter not in user_word:
       temp = list(user_word)
       for i in range(len(guess_word)):
         if guess_word[i] == letter:
           temp[i] = letter
       user_word = "".join(temp)
+      message = "Well guessed!"
     else:
-      if letter in user_word:
-        print("You have already entered this letter")
-      else:
+      if letter not in user_word:
         index += 1
-        if index < len(HANGMANPICS):
-          print(f"\nWrong guess!\n{HANGMANPICS[index]}")
+        message = "Wrong guess!"
+      else:
+        message = f"You have already guessed: {letter}"
+    print_game_state(message, HANGMANPICS[index], user_word)
+    
 
   if user_word == guess_word:
-    print(f"""You have found the correct word: "{guess_word}"\n""")
+    message = f"""You have found the correct word: "{guess_word}"\n"""
+    print_game_state(message, HANGMANPICS[index], user_word)
   else:
-    print(f"""You lost the game. The correct word was "{guess_word}"\n""")
+    message = f"""You lost the game. The correct word was "{guess_word}"\n"""
+    print_game_state(message, HANGMANPICS[index], user_word)
 
 if __name__=="__main__":
   game()
